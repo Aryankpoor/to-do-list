e704deadb387eef0fd94e685d77afb8118a4c13b
Aryankpoor <Kapooraryan0419@gmail.com>
2024-06-29 16:48:51 +0530

    add todos array functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,56 @@ import './App.css';
 
 export default function App() {
   const [newItem, setNewItem] = useState("")
- // setNewItem("sfdsd")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos((currentTodos) => {
+    return [
+      ...currentTodos,
+       { id: crypto.randomUUID(), title: newItem, completed:
+       false},
+
+    ]
+    }) 
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false }
+      ]
+    })
+    
+  }
+
+
   return (
+
   <>
-  <form className='new-item-form'>
+
+  <form onSubmit={handleSubmit} className='new-item-form'>
+
     <div className="form-row">
       <label htmlFor='item'>New Item</label>
-      <input type="text" id="item"/>
+      <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"/>
     </div>
+
     <button className='btn'>Add</button>
+
   </form>
+
   <h1 className='header'>To do list</h1>
+
+
   <ul className='list'>
+
     <li>
       <label>
         <input type="checkbox" />
         item 1
       </label>
       <button className='btn btn-danger'>Delete</button>
+
       <label>
         <input type="checkbox" />
         item 2
