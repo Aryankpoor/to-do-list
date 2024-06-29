fff8fc866576bc3f8b399e6cf0af96fb792a5b09
Aryankpoor <Kapooraryan0419@gmail.com>
2024-06-29 16:25:45 +0530

    Added useState property

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import './App.css';
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+ // setNewItem("sfdsd")
   return (
   <>
   <form className='new-item-form'>
@@ -10,6 +13,21 @@ export default function App() {
     </div>
     <button className='btn'>Add</button>
   </form>
-  <h1 className='header'>To do list</h1></>
+  <h1 className='header'>To do list</h1>
+  <ul className='list'>
+    <li>
+      <label>
+        <input type="checkbox" />
+        item 1
+      </label>
+      <button className='btn btn-danger'>Delete</button>
+      <label>
+        <input type="checkbox" />
+        item 2
+      </label>
+      <button className='btn btn-danger'>Delete</button>
+    </li>
+  </ul>
+  </>
   )
 }
